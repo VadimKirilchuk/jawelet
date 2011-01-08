1c52e9c3e6b14628a207bb0050aae22d9442aeed
KirilchukVadim <KirilchukVadim@fcd84088-caa2-c9e6-991d-e86a68098f49>
2011-01-08 15:53:03 +0000

    Added menu to gui. Cleaned main method in gui package.

--- trunk/src/ru/ifmo/diplom/kirilchuk/jawelet/gui/JaweletMainFrame.java
+++ trunk/src/ru/ifmo/diplom/kirilchuk/jawelet/gui/JaweletMainFrame.java
@@ -4,68 +4,126 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 
 import ru.ifmo.diplom.kirilchuk.jawelet.core.dwt.transforms.DWTransform2D;
 import ru.ifmo.diplom.kirilchuk.jawelet.core.dwt.transforms.legall.impl.LeGallWaveletTransform;
 import ru.ifmo.diplom.kirilchuk.jawelet.gui.util.ImageUtils;
+import ru.ifmo.diplom.kirilchuk.jawelet.gui.util.SwingUtils;
 
 /**
- *
+ * 
  * @author Kirilchuk V.E.
  */
 public class JaweletMainFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 
-    private final int width  = 800;
-    private final int heigth = 600;
+	private final int width  = 800;
+	private final int heigth = 600;
 
-    private final JLabel  imageLabel = new JLabel();
-    private final JButton decomposeButton   = new JButton("Decompose");
-    private final JButton reconstructButton = new JButton("Reconstruct");
-    
-    
-    private BufferedImage image;
+	private final JLabel imageLabel = new JLabel();
+	private final JButton decomposeButton = new JButton("Decompose");
+	private final JButton reconstructButton = new JButton("Reconstruct");
 
-    public JaweletMainFrame() {
-        super("Jawelet");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(width, heigth);
+	private BufferedImage image;
 
-        initAndLayout();
+	public JaweletMainFrame() {
+		super("Jawelet");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(width, heigth);
 
-        setLocationRelativeTo(null);
-        setVisible(true);
-    }
+		initMenu();
+		initAndLayout();
+
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
 
 	private void initAndLayout() {
 		setLayout(new BorderLayout());
-        add(new JScrollPane(imageLabel), BorderLayout.CENTER);
-        
-        Box rightBox = Box.createVerticalBox();
-        rightBox.add(Box.createVerticalGlue());
-        
-        decomposeButton.addActionListener(new TransformButtonListener());               
-        reconstructButton.addActionListener(new TransformButtonListener());
-        
-        rightBox.add(decomposeButton);
-        rightBox.add(reconstructButton);
-        
-        rightBox.add(Box.createVerticalStrut(20));
-        add(rightBox, BorderLayout.EAST);
+		add(new JScrollPane(imageLabel), BorderLayout.CENTER);
+
+		Box rightBox = Box.createVerticalBox();
+		rightBox.add(Box.createVerticalGlue());
+
+		TransformButtonListener transformButtonListener = new TransformButtonListener();
+		decomposeButton.addActionListener(transformButtonListener);
+		reconstructButton.addActionListener(transformButtonListener);
+
+		rightBox.add(decomposeButton);
+		rightBox.add(reconstructButton);
+
+		rightBox.add(Box.createVerticalStrut(20));
+		add(rightBox, BorderLayout.EAST);
 	}
 
-    public void setImage(BufferedImage image) {
-    	this.image = image;
-        imageLabel.setIcon(new ImageIcon(image));
-    }
-    
+	public void setImage(BufferedImage image) {
+		this.image = image;
+		imageLabel.setIcon(new ImageIcon(image));
+	}
+
+	private void initMenu() {
+		JMenuBar menuBar = new JMenuBar();
+
+		JMenu fileMenu = new JMenu("File");
+
+		JMenuItem loadItem = new JMenuItem("Load image");
+		loadItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JaweletMainFrame gui = JaweletMainFrame.this;
+				try {
+					File file = SwingUtils.chooseImageFile(gui);
+					if (file == null) {
+						SwingUtils.showError(gui, "You didn`t select any file.");
+						return;
+					}
+
+					BufferedImage image = ImageUtils.loadImage(file);
+					if (image == null) {
+						SwingUtils.showError(gui, "Image load failed.");
+						return;
+					}
+
+					/* autoconvert to grayscale */
+					image = ImageUtils.tryCreateGrayscaleCopy(image);
+					gui.setImage(image);
+				} catch (IOException ex) {
+					SwingUtils.showError(gui, ex.getMessage());
+				}
+			}
+		});
+		fileMenu.add(loadItem);
+		fileMenu.add(new JSeparator());
+		
+		JMenuItem exitItem = new JMenuItem("Exit");
+		exitItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JaweletMainFrame.this.dispose();
+			}
+		});
+		fileMenu.add(exitItem);
+
+		menuBar.add(fileMenu);
+		setJMenuBar(menuBar);
+	}
+	
+
 	private class TransformButtonListener implements ActionListener {
 		private final DWTransform2D transform = new DWTransform2D(new LeGallWaveletTransform());
 
@@ -93,7 +151,7 @@ public class JaweletMainFrame extends JFrame {
 					}
 				}
 			}
-			
+
 		}
 	}
 }
