a42149ec83fe32ecf6b15d8c82297e2d06e1b620
KirilchukVadim <KirilchukVadim@fcd84088-caa2-c9e6-991d-e86a68098f49>
2011-01-08 15:53:03 +0000

    Added menu to gui. Cleaned main method in gui package.

--- src/ru/ifmo/diplom/kirilchuk/jawelet/gui/Main.java
+++ src/ru/ifmo/diplom/kirilchuk/jawelet/gui/Main.java
@@ -1,60 +1,20 @@
 package ru.ifmo.diplom.kirilchuk.jawelet.gui;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
-import ru.ifmo.diplom.kirilchuk.jawelet.gui.util.ImageUtils;
-import ru.ifmo.diplom.kirilchuk.jawelet.gui.util.SwingUtils;
-
 /**
- *
+ * 
  * @author Kirilchuk V.E.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
 
-            @Override
-            public void run() {
-                JaweletMainFrame gui = new JaweletMainFrame();
-                try {
-                    File file = chooseImageFile(gui);
-                    if (file == null) {
-                    	SwingUtils.showError(gui, "You didn`t select any file.", true);
-                    	gui.dispose();
-                    	return;
-                    }
-                    
-                    BufferedImage image = ImageUtils.loadImage(file);
-                    if (image == null) {
-                        SwingUtils.showError(gui, "Image load failed.", true);
-                        gui.dispose();
-                        return;
-					}
-                    
-					/* autoconvert to grayscale */
-					image = ImageUtils.tryCreateGrayscaleCopy(image);
-					gui.setImage(image);
-
-                } catch (IOException ex) {
-                	SwingUtils.showError(gui, ex.getMessage(), true);
-                }
-            }
-
-			private File chooseImageFile(JaweletMainFrame gui) {
-				JFileChooser fileChooser = new JFileChooser();
-				fileChooser.showOpenDialog(gui);
-				
-				File file = fileChooser.getSelectedFile();
-				return file;
+			@Override
+			public void run() {
+				new JaweletMainFrame();
 			}
-        });
-    }
-
-
+		});
+	}
 }
--- src/ru/ifmo/diplom/kirilchuk/jawelet/gui/util/SwingUtils.java
+++ src/ru/ifmo/diplom/kirilchuk/jawelet/gui/util/SwingUtils.java
@@ -1,17 +1,45 @@
 package ru.ifmo.diplom.kirilchuk.jawelet.gui.util;
 
 import java.awt.Component;
+import java.io.File;
+
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
 
 /**
- *
+ * 
  * @author Kirilchuk V.E.
  */
 public final class SwingUtils {
-    private SwingUtils() {}
+	private SwingUtils() {
+	}
 
-    public  static void showError(Component parent, String message, boolean termination) {
-        String msg = termination ? message.concat("\nProgram will terminate.") : message;
-        JOptionPane.showMessageDialog(parent, msg, "Error", JOptionPane.ERROR_MESSAGE);
-    }
+	public static void showError(Component parent, String message) {
+		JOptionPane.showMessageDialog(parent, message, "Error", JOptionPane.ERROR_MESSAGE);
+	}
+
+	public static File chooseImageFile(Component parent) {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileFilter(new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "Supported image files(JPEG, PNG, BMP)";
+			}
+
+			@Override
+			public boolean accept(File f) {
+				String name = f.getName();
+				boolean accepted = f.isDirectory()  
+						|| name.endsWith(".jpg") 
+						|| name.endsWith(".jpeg") 
+						|| name.endsWith(".png")
+						|| name.endsWith(".bmp");
+				return accepted;
+			}
+		});
+
+		fileChooser.showOpenDialog(parent);
+		return fileChooser.getSelectedFile();
+	}
 }
